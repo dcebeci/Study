a4a477bc2c09e836f318dd40f26203ec4d3103b6
Doğukan Cebeci <56614064+dcebeci@users.noreply.github.com>
2025-11-06 01:19:48 +0300

    search-bar created.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,15 +3,17 @@ import { CardListComponent } from '../../components/card-list/card-list.componen
 import { CommonModule } from '@angular/common';
 import { Router} from '@angular/router';
 import { UserService } from '../../services/user.service';
-import { catchError, finalize, map, Observable, of, tap } from 'rxjs';
+import { BehaviorSubject, catchError, combineLatest, finalize, map, Observable, of, tap } from 'rxjs';
 import { User } from '../../models/user.model';
 import { CardItem } from '../../models/card-item.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   imports: [ 
     CommonModule, 
     CardListComponent,
+    FormsModule
    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -23,27 +25,50 @@ export class HomeComponent implements OnInit {
   users$!: Observable<CardItem[]>;
   loading = false;
   errorMessage: string | null = null;
+  private searchTerm$ = new BehaviorSubject<string>('');
+  private usersSource$ = new BehaviorSubject<User[]>([]); 
+  searchTerm: string = ''; 
 
   ngOnInit(): void {
+    this.setupFilteredUsers();
     this.loadUsers();
   }
-
+  onSearchTermChange(newTerm: string): void {
+    this.searchTerm$.next(newTerm);
+  }
+   private setupFilteredUsers(): void {
+    this.users$ = combineLatest([
+      this.usersSource$,
+      this.searchTerm$
+    ]).pipe(
+      map(([users, term]) => {
+        const lowerTerm = term.toLowerCase().trim();
+        const filteredUsers = lowerTerm
+          ? users.filter(user => user.name.toLowerCase().includes(lowerTerm))
+          : users;
+        return filteredUsers.map(u => this.toCardItem(u));
+      }),
+      catchError((err) => {
+        console.error('Veri akışında hata:', err);
+        this.errorMessage = 'Veriler işlenirken bir hata oluştu.';
+        return of([] as CardItem[]);
+      })
+    );
+  }
   private loadUsers(): void {
     this.loading = true;
     this.errorMessage = null;
-    this.users$ = this.userService.getUsers().pipe(
-      tap(() => console.log('API isteği atıldı')),
-      map((users: User[]) =>
-        users.map((u) => this.toCardItem(u))
-      ),
+    this.userService.getUsers().pipe(
       finalize(() => (this.loading = false)),
       catchError((err) => {
         console.error('Kullanıcı verisi yüklenirken hata:', err);
         this.errorMessage = 'Kullanıcı verileri yüklenirken bir hata oluştu.';
         this.loading = false; 
-        return of([] as CardItem[]);
+        return of([] as User[]);
       })
-    );
+    ).subscribe(users => {
+      this.usersSource$.next(users);
+    });
   }
 
   private toCardItem(u: User): CardItem {
