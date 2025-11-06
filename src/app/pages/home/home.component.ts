a8a5d9929c626865ed1d8de895f52027f52e5cbe
Doğukan Cebeci <56614064+dcebeci@users.noreply.github.com>
2025-11-06 13:10:56 +0300

    unused value removed.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,7 @@ import { CardListComponent } from '../../components/card-list/card-list.componen
 import { CommonModule } from '@angular/common';
 import { Router} from '@angular/router';
 import { UserService } from '../../services/user.service';
-import { BehaviorSubject, catchError, combineLatest, finalize, map, Observable, of, tap } from 'rxjs';
+import { BehaviorSubject, catchError, combineLatest, finalize, map, Observable, of } from 'rxjs';
 import { User } from '../../models/user.model';
 import { CardItem } from '../../models/card-item.model';
 import { FormsModule } from '@angular/forms';
