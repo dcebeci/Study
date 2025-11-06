27b281100743830a86269ee35ba0332ed1c114df
Doğukan Cebeci <56614064+dcebeci@users.noreply.github.com>
2025-11-06 15:24:17 +0300

    footer added & responsiveness arranged.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SpinnerComponent,HeaderComponent],
+  imports: [RouterOutlet,SpinnerComponent,HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
